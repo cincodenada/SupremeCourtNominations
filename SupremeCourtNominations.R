ca23b6501f8a9e6d1667c0fe0a3de5835b26bfa1
Joel Bradshaw <cincodenada@gmail.com>
2016-02-14 10:45:47 -0800

    Initial graphing of things
    Remove inaugural positions, fix dates

--- SupremeCourtNominations.R
+++ SupremeCourtNominations.R
@@ -10,14 +10,22 @@ makedate = function(frame, cols) {
     for(col in cols) {
         frame[,col] = as.POSIXct(as.Date(frame[,col], "%B %e, %Y"))
     }
+
+    frame
 }
 
-makedate(succession, 'Date')
-makedate(nominations, list('Submission.Date','Result.Date'))
+succession = makedate(succession, 'Date')
+nominations = makedate(nominations, list('Submission.Date','Result.Date'))
 
 succession = subset(succession, Action != 'Oath of office')
+nominations = subset(nominations, Replacing != 'Inaugural')
+
 # Tried to do tail(strsplit(x," ")) instead of the second gsub, but...shit got cray
 succession$Last.Name = sapply(succession$Justice, function(x) { x=gsub("(, \\w+\\.| I+)$","",x); regmatches(x,regexpr("((?:Van )?\\S+)$",x)) })
 colnames(succession) <- c('Vacancy.Date','Vacancy.Name','Vacancy.Reason','Last.Name')
 
 joined = merge(nominations,succession,by.x='Replacing',by.y='Last.Name',all.x=T)
+
+p = ggplot(joined) +
+    geom_segment(aes(x=Vacancy.Date,xend=Submission.Date,y=Submission.Date,yend=Submission.Date),color="blue",size=2) +
+    geom_segment(aes(x=Submission.Date,xend=Result.Date,y=Submission.Date,yend=Submission.Date),color="green",size=2)
