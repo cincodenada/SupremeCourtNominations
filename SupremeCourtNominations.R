54ef3a07ab83fabc659b72afccb2e514ed3543c6
Joel Bradshaw <cincodenada@gmail.com>
2016-02-14 14:17:22 -0800

    Deal with dates a bit more nicely

--- SupremeCourtNominations.R
+++ SupremeCourtNominations.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-succession = read.csv('succession.csv',stringsAsFactors=F)
+succession = read.csv('succession.csv',stringsAsFactors=F,col.names=c('Vacancy.Date','Vacancy.Name','Vacancy.Reason'))
 nominations = read.csv('nominations.csv')
 
 makedate = function(frame, cols) {
@@ -8,21 +8,20 @@ makedate = function(frame, cols) {
         cols = list(cols)
     }
     for(col in cols) {
-        frame[,col] = as.POSIXct(as.Date(frame[,col], "%B %e, %Y"))
+        frame[,paste(col,".orig",sep="")] = frame[,col]
+        frame[,col] = as.Date(frame[,col], "%B %e, %Y")
     }
-
     frame
 }
 
-succession = makedate(succession, 'Date')
+succession = makedate(succession, 'Vacancy.Date')
 nominations = makedate(nominations, list('Submission.Date','Result.Date'))
 
-succession = subset(succession, Action != 'Oath of office')
+succession = subset(succession, Vacancy.Reason != 'Oath of office')
 nominations = subset(nominations, Replacing != 'Inaugural')
 
 # Tried to do tail(strsplit(x," ")) instead of the second gsub, but...shit got cray
-succession$Last.Name = sapply(succession$Justice, function(x) { x=gsub("(, \\w+\\.| I+)$","",x); regmatches(x,regexpr("((?:Van )?\\S+)$",x)) })
-colnames(succession) <- c('Vacancy.Date','Vacancy.Name','Vacancy.Reason','Last.Name')
+succession$Last.Name = sapply(succession$Vacancy.Name, function(x) { x=gsub("(, \\w+\\.| I+)$","",x); regmatches(x,regexpr("((?:Van )?\\S+)$",x)) })
 
 joined = merge(nominations,succession,by.x='Replacing',by.y='Last.Name',all.x=T)
 
