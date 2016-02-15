99c4910b78e22313033fb708440f29abb3046332
Joel Bradshaw <cincodenada@gmail.com>
2016-02-14 18:29:50 -0800

    Eliminate promotions
    Consistify data a bit

--- SupremeCourtNominations.R
+++ SupremeCourtNominations.R
@@ -22,6 +22,7 @@ succession = makedate(succession, 'Vacancy.Date')
 nominations = makedate(nominations, list('Submission.Date','Result.Date'))
 
 succession = subset(succession, Vacancy.Reason != 'Oath of office')
+promotions = subset(succession, Vacancy.Reason == 'Promoted')
 succession = subset(succession, Vacancy.Reason != 'Promoted')
 nominations = subset(nominations, Replacing != 'Inaugural')
 nominations = subset(nominations, Result != 'no action')
@@ -31,7 +32,10 @@ succession$Last.Name = sapply(succession$Vacancy.Name, function(x) { x=gsub("(, 
 nominations$Last.Name = sapply(nominations$Nominee, function(x) { x=gsub("(, \\w+\\.| I+)$","",x); regmatches(x,regexpr("((?:Van )?\\S+)$",x)) })
 
 #joined = merge(nominations,succession,by.x='Replacing',by.y='Last.Name',all.x=T)
-joined = nominations
+promotions$YearNominee = paste(format(promotions$Vacancy.Date, '%Y'), promotions$Vacancy.Name)
+nominations$YearNominee = paste(format(nominations$Result.Date, '%Y'), nominations$Nominee)
+joined = merge(nominations, promotions, all.x=T)
+joined = subset(joined, is.na(Vacancy.Reason))
 
 #joined$Nomination.Time = as.numeric(joined$Submission.Date - joined$Vacancy.Date)/365
 joined$Result.Time = as.numeric(joined$Result.Date - joined$Submission.Date)/365
@@ -48,12 +52,12 @@ joined = with(joined, joined[order(Submission.Term),])
 joined$rownum = 1:nrow(joined)
 
 p = ggplot(joined) +
-    geom_rect(ymin=98,ymax=nrow(joined),xmin=maketerm(as.Date("2015-11-01")),xmax=maketerm(as.Date("2016-11-01")),color="transparent",fill="gray",alpha=0.5) +
+    geom_rect(ymin=95,ymax=nrow(joined),xmin=maketerm(as.Date("2015-11-01")),xmax=maketerm(as.Date("2016-11-01")),color="transparent",fill="gray",alpha=0.5) +
     geom_vline(xintercept=maketerm(as.Date("2016-02-13")),color="red") +
     geom_segment(aes(x=Submission.Term,xend=Result.Term,y=rownum,yend=rownum,color=Result),size=2) +
     geom_text(aes(label=Label, y=rownum,x=Result.Term),hjust=-0.025,size=2.5) +
     scale_x_continuous(expand=c(0,0),limits=c(2.5,4.5)) +
-    scale_y_continuous(breaks=c(),limits=c(99,NA)) +
+    scale_y_continuous(breaks=c(),limits=c(96,NA)) +
     labs(title="Historical Timing of US Supreme Court Nominations Relative to Presidential Term\n", x="Years into term", y="")
 
 png('Nominations.png',w=1000,h=500,res=90)
